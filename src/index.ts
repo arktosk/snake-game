7699181c47b9477f8eeced397ed27a566fe9a9d4
a.krauzowicz <a.krauzowicz@zoovu.com>
2020-11-13 23:52:51 +0100

    Create canvas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 console.log("Hello World!");
+const canvas: HTMLCanvasElement = document.createElement("canvas");
+canvas.width = 625;
+canvas.height = 625;
+
+document.body.insertBefore(canvas, document.body.firstChild);
